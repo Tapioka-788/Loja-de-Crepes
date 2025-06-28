f0d73d9a347b3e8d57bc0c38ddba4533375746f3
Tapioka-788 <guilhermezuchellifelipetto@gmail.com>
2025-06-28 11:28:22 -0300

    Correcao nos Banners

--- Controller/index/index.js
+++ Controller/index/index.js
@@ -14,3 +14,19 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 window.trocarBanner = trocarBanner
+
+
+function trocarImagensParaMobile() {
+    if (window.innerWidth <= 900) {
+        document.getElementById("img1").src = "./../../Model/francaPe.jpeg";
+        document.getElementById("img2").src = "./../../Model/pe.png";
+        document.getElementById("img3").src = "./../../Model/fullStackpe.jpeg";
+        document.getElementById("img4").src = "./../../Model/docepe.jpeg";
+    }
+}
+
+// Executa ao carregar a página
+window.addEventListener("load", trocarImagensParaMobile);
+
+// Também executa se a pessoa redimensionar a tela
+window.addEventListener("resize", trocarImagensParaMobile);
